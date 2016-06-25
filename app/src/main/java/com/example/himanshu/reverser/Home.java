cc744050936ec88173ae7aab18261c60f20ae99b
theankurrajput <ankurrajput@outlook.com>
2016-06-26 00:34:58 +0530

    First commit

---
+++ app/src/main/java/com/example/himanshu/reverser/Home.java
@@ -0,0 +1,23 @@
+package com.example.himanshu.reverser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+}
